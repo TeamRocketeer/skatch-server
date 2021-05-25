e86e725788cc91c5abad0bf5fa4ad482270d9daf
Noaarhk <skarb8277@naver.com>
2021-05-25 14:00:54 +0900

    initialize

--- skatch/user/models.py
+++ skatch/user/models.py
@@ -16,20 +16,3 @@ class User(AbstractBaseUser):
     # 필수로 작성해야하는 field
     REQUIRED_FIELDS = ['username', 'password']
 
-#
-#
-# class Profile(models.Model):
-#         # related fields =====
-#     user = models.OneToOneField('account.User', on_delete=models.CASCADE, related_name='profile')
-#     # favorite_exercises = models.ManyToManyField('fitnessdiving.CourseCategory', related_name='profiles')
-#
-#     # required fields =====
-#     phone = models.CharField(max_length=20, null=True)
-#     name = models.CharField(max_length=10, null=True)
-#     nickname = models.CharField(max_length=10, null=True)
-#     # option fields =====
-#     age_range = models.IntegerField(choices=AGE_RANGE, blank=True, null=True)
-#     gender = models.IntegerField(choices=GENDER, blank=True, null=True, help_text='1: 여성 , 2: 남성')
-#     avatar = models.ImageField(upload_to=profile_avatar_path, default='', blank=True, null=True)
-#     one_line_intro = models.CharField(max_length=100, default='', blank=True, null=True)
-#     birthday = models.DateField(blank=True, null=True)
